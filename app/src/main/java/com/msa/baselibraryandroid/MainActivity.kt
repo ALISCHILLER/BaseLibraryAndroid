a2479520f5dd3b7d1a490542a4f77f83ddca748b
ZARMACARON\a-soleymani <Solimaniali90@gmail.com>
2022-07-30 17:48:07 +0430

    base toast

--- app/src/main/java/com/msa/baselibraryandroid/MainActivity.kt
+++ app/src/main/java/com/msa/baselibraryandroid/MainActivity.kt
@@ -2,10 +2,25 @@ package com.msa.baselibraryandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.msa.base_msa_library.weight_android.toast.BaseToastStyle
+import com.msa.base_msa_library.weight_android.toast.Base_Toast
+import com.msa.base_msa_library.weight_android.toast.Base_Toast.Companion.GRAVITY_BOTTOM
+import com.msa.base_msa_library.weight_android.toast.Base_Toast.Companion.LONG_DURATION
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Base_Toast.createToast(
+            this@MainActivity,
+            "Hurray success 😍",
+            "Upload Completed successfully!",
+            BaseToastStyle.SUCCESS,
+            Base_Toast.GRAVITY_BOTTOM,
+            Base_Toast.LONG_DURATION,
+            ResourcesCompat.getFont(this, com.msa.base_msa_library.R.font.helvetica_regular)
+            )
     }
 }
---
+++ base_msa_library/src/main/java/com/msa/base_msa_library/weight_android/toast/BaseToastStyle.kt
@@ -0,0 +1,13 @@
+package com.msa.base_msa_library.weight_android.toast
+
+enum class BaseToastStyle {
+    SUCCESS, ERROR, WARNING, INFO, DELETE, NO_INTERNET;
+
+    fun getName(): String {
+        if (this.name.contains("_")) {
+            return this.name.replaceFirst("_", " ")
+        }
+        return this.name
+    }
+
+}
